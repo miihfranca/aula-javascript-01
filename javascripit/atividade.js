5c2e93ec7e1a7c9d98aa4718801a58ab772cdac6
michele franca <85418317+miihfranca@users.noreply.github.com>
2021-09-24 12:39:05 -0300

    Add files via upload

---
+++ javascripit/atividade.js
@@ -0,0 +1,45 @@
+//atividade Criar variáveis com tipos de dados
+
+//Number
+
+var numero =  50;
+console.log(numero);
+
+// String
+
+var gmail = 'michelefdf@gmail.com';
+console.log(gmail)
+
+// Boolean
+var verdadeiro = true;
+var falso =  false;
+console.log("michele fez as atividades");
+console.log(verdadeiro)
+console.log("michele atualizou as atividades");
+console.log(false)
+
+//Array
+var maquiagem = [
+              "primer", 
+              "sombra",
+              "baton"
+              ];
+console.log(maquiagem)
+
+//Object
+var pele = {
+  base:"marron escuro",
+  marca:"rubbi  rose",
+  valor: 20,
+  ehboa: true    
+};
+console.log(pele.ehboa) 
+
+//Undefined
+ var sem_valor = undefined;
+console.log("sem_valor:" + sem_valor );
+
+//nul 
+
+var nulo = null;
+console.log ("nulo:" + nulo );
---
+++ javascripit/atividade2.js
@@ -0,0 +1,24 @@
+// 1)      Criar duas variáveis usando a palavra-chave var.
+
+var nome_completo = "michele fernanda de franca";
+console.log(nome_completo);
+
+var cidade = "recife";
+console.log(cidade);
+
+//2)      Criar duas variáveis usando a palavra-chave let.
+
+let idade = 23;
+console.log(idade);
+
+let data_de_nascimento = '02/11/1997';
+console.log(data_de_nascimento);
+
+//3)      Criar duas constantes usando a palavra-chave const.
+
+const nascimento = 1997;
+console.log(nascimento);
+
+const novembro = ["mes", 11];
+console.log(novembro)
+
